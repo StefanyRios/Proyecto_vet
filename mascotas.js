26a8a27409498a49e021aaab7c0cf42929f4d0af
StefanyRios <farime85@gmail.com>
2023-05-12 20:53:33 -0500

    .

--- mascotas.js
+++ mascotas.js
@@ -1,46 +1,31 @@
+
 let tablaMascotas = document.getElementById("tabla-mascotas")
 let cuerpoTabla = tablaMascotas.querySelector("tbody");
 
+fetch('datos_mascotas.json') //solicitud 
+    .then(Response => Response.json())// la respuesta de la solicitud -- objeto 
+    //objeto que se convirtio 
+    .then(mascotas => {
+        for (let i = 0; i < mascotas.longitud; i++) {
+            let mascota = mascotas[i];
 
-//Clase
+            let fila = cuerpoTabla.insertRow();
+            let celdaNombreMascota = fila.insertCell();
+            let celdaNombreDueño = fila.insertCell();
+            let celdaCedulaDueño = fila.insertCell();
+            let celdaEdadMascota = fila.insertCell();
+            let celdaTelefonoDueño = fila.insertCell();
+            let celdaEspecialidad = fila.insertCell();
+            let celdaMedico = fila.insertCell();
 
-class Mascota {
-    constructor(nombreMascotas, nombreDueño, cedulaDueño, edadMascota, telefonoDueño, especialidad, medico) {
-        this.nombreMascotas = nombreMascotas;
-        this.nombreDueño = nombreDueño;
-        this.cedulaDueño = cedulaDueño;
-        this.edadMascota = edadMascota;
-        this.telefonoDueño = telefonoDueño;
-        this.especialidad = especialidad;
-        this.medico = medico;
-
-
-    }
-} 
-
-//Objeto
-let mascota1=new Mascota ("luna","Ana","1","5 año","3105678","Veterinan Conductual","Jorge")
-let mascota2=new Mascota ("orion","Jorge","2","2 meses","31055555","Medicina Interna","Daniela")
-
-let mascotas=[mascota1,mascota2] 
-
-
-for (let i = 0; i < mascotas.length; i++) {
-    let mascota = mascotas[i];
-    let fila = cuerpoTabla.insertRow();
-    let celdaNombreMascota = fila.insertCell();
-    let celdaNombreDueño = fila.insertCell();
-    let celdaCedulaDueño = fila.insertCell();
-    let celdaedadMascota = fila.insertCell();
-    let celdatelefonoDueño = fila.insertCell();
-    let celdaespecialidad = fila.insertCell();
-    let celdamedico = fila.insertCell();
-
-    celdaNombreMascota.textContent = mascota.nombreMascotas;
-    celdaNombreDueño.textContent = mascota.nombreDueño;
-    celdaCedulaDueño.textContent = mascota.cedulaDueño;
-    celdaedadMascota.textContent = mascota.edadMascota;
-    celdatelefonoDueño.textContent = mascota.telefonoDueño;
-    celdaespecialidad.textContent = mascota.especialidad;
-    celdamedico.textContent = mascota.medico;
-}
+            celdaNombreMascota.textContent = mascota.nombreMascota;
+            celdaNombreDueño.textContent = mascota.nombreDueño;
+            celdaCedulaDueño.textContent = mascota.cedulaDueño;
+            celdaEdadMascota.textContent = mascota.edadMascota;
+            celdaTelefonoDueño.textContent = mascota.telefonoDueño;
+            celdaEspecialidad.textContent = mascota.especialidad;
+            celdaMedico.textContent = mascota.medico;
+        }
+    })//control de errores 
+    .catch(console.log("No encontro el documento"));
+  
